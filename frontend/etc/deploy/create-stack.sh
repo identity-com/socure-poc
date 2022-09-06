77be2f7cb2f673145e319eed370c57a62c54f3d9
William Brooks <william@identity.org>
2022-09-06 09:42:03 +0200

    Updated deployment parameters

--- frontend/etc/deploy/create-stack.sh
+++ frontend/etc/deploy/create-stack.sh
@@ -17,16 +17,16 @@ STAGE=$1
 
 # the ARN of the *.identity.com certificate in prod
 #PROD_ACCOUNT_CERTIFICATE_ARN="arn:aws:acm:us-east-1:146055947386:certificate/23b59743-d806-4899-ab04-9a807238a4d4"
-PROD_ACCOUNT_CERTIFICATE_ARN="??"
+PROD_ACCOUNT_CERTIFICATE_ARN="arn:aws:acm:us-east-1:146055947386:certificate/23b59743-d806-4899-ab04-9a807238a4d4"
 # the ARN of the *.identity.com certificate in dev
 DEV_ACCOUNT_CERTIFICATE_ARN="arn:aws:acm:us-east-1:159876458955:certificate/aa27de1d-f254-4953-9f55-73c2543825ee"
 
-[[ $STAGE = "prod" ]] && ACCESS_IDENTITY="?" || ACCESS_IDENTITY="?"
-[[ $STAGE = "prod" ]] && HOSTED_ZONE_ID="?" || HOSTED_ZONE_ID="Z012078129CDW83TJ2O5Q"
-[[ $STAGE = "prod" ]] && CERTIFICATE_ARN="${PROD_ACCOUNT_CERTIFICATE_ARN}" || CERTIFICATE_ARN="${PROD_ACCOUNT_CERTIFICATE_ARN}"
+[[ $STAGE = "prod" ]] && ACCESS_IDENTITY="E13VH96WYU0CFA" || ACCESS_IDENTITY="?"
+[[ $STAGE = "prod" ]] && HOSTED_ZONE_ID="Z01073574EZAII1CFU1I" || HOSTED_ZONE_ID="Z012078129CDW83TJ2O5Q"
+[[ $STAGE = "prod" ]] && CERTIFICATE_ARN="${PROD_ACCOUNT_CERTIFICATE_ARN}" || CERTIFICATE_ARN="${DEV_ACCOUNT_CERTIFICATE_ARN}"
 
-[[ $STAGE = "prod" ]] && DOMAIN_PREFIX="verifier" || DOMAIN_PREFIX="verifier-${STAGE}"
-PROJECT="SocureVerifier"
+[[ $STAGE = "prod" ]] && DOMAIN_PREFIX="socure-demo" || DOMAIN_PREFIX="socure-demo-${STAGE}"
+PROJECT="SocureDemo"
 WEBSITE_NAME="${DOMAIN_PREFIX}.identity.com"
 
 aws cloudformation create-stack --stack-name ${DOMAIN_PREFIX}-identity-com \
