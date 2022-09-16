e9346ecbdbc5f4daca3461a577ecc28ac3425eca
William Brooks <william@identity.org>
2022-09-16 10:27:35 +0200

    fix: linting and console.log cleanup

--- frontend/src/iframe/Socure.tsx
+++ frontend/src/iframe/Socure.tsx
@@ -1,6 +1,5 @@
 import {useEffect, useState} from "react";
 import {Navigate, useParams} from 'react-router-dom';
-import axios from "axios";
 
 declare const SocureInitializer: any;
 const API_KEY = 'adcd2fe2-b49c-4d20-ba72-4fa582c2c53d';
@@ -13,7 +12,6 @@ export default function Socure() {
     const start = async () => {
         var config = {
             onProgress: async (response: any) => {
-                // axios.post('https://socure-demo-api-identity-com.relay.evervault.com/result', response);
                 console.log("=== onProgress: Start");
                 console.log(response);
                 console.log("=== onProgress: Start");
@@ -21,7 +19,6 @@ export default function Socure() {
             onSuccess: async (response: any) => {
                 response.token = token;
 
-                // await axios.post('https://localhost:3030/result', response);
                 console.log("=== onSuccess: Start");
                 console.log(response);
                 console.log("=== onSuccess: End");
@@ -31,8 +28,9 @@ export default function Socure() {
                 },"*" );
             },
             onError: async (response: any) => {
-                console.log("error");
+                console.log("=== onError: Start");
                 console.log(response);
+                console.log("=== onError: End");
             },
 
             qrCodeNeeded: true
@@ -40,9 +38,7 @@ export default function Socure() {
 
         SocureInitializer.init(API_KEY)
             .then((lib: { init: (arg0: string, arg1: string, arg2: { onProgress: (progress: any) => void; onSuccess: (success: any) => void; onError: (error: any) => void; qrCodeNeeded: boolean; }) => Promise<any>; start: (arg0: number, p?: { customerUserId: string | undefined }) => Promise<any>; }) => {
-                console.log("API KEY INIT");
                 lib.init(API_KEY, "#socure", config).then(function () {
-                    console.log("``lib init`")
                     lib.start(2, {customerUserId: token}).then(function (response: any) {
                             console.log('response: START');
                             console.log(response);
