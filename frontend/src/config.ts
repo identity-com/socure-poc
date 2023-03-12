32a4ef8f6712b9de7115f9859486dc3368c9d757
julian <julian@identity.org>
2023-03-12 12:14:06 +0200

    frontend: Add device fingerprinting

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,6 @@
 export default {
     socure: {
-        api_key: 'adcd2fe2-b49c-4d20-ba72-4fa582c2c53d'
+        api_key: '',
+        sdk_key: ''
     }
 }
--- frontend/src/iframe/Socure.tsx
+++ frontend/src/iframe/Socure.tsx
@@ -2,73 +2,97 @@ import {useEffect, useState} from "react";
 import {Navigate, useParams} from 'react-router-dom';
 import config from '../config';
 
+// TODO(): Type SocureInitializer and  devicer
 declare const SocureInitializer: any;
+declare const devicer: any;
 const API_KEY = config.socure.api_key;
+const SDK_KEY = config.socure.sdk_key;
 
 export default function Socure() {
-    let { token } = useParams();
+  let {token} = useParams();
 
-    let [confirmed] = useState(false);
+  let [confirmed] = useState(false);
 
-    const start = async () => {
-        var config = {
-            onProgress: async (response: any) => {
-                console.log("=== onProgress: Start");
-                console.log(response);
-                console.log("=== onProgress: Start");
-            },
-            onSuccess: async (response: any) => {
-                response.token = token;
+  const start = () => {
+    let deviceFPOptions = {
+      publicKey: SDK_KEY,
+      userConsent: true,
+      context: 'KYC'
+    };
+    // TODO(): Type response from devicer.run
+    devicer.run(deviceFPOptions, (response: any) => initSocure(response.deviceSessionId));
+  }
 
-                console.log("=== onSuccess: Start");
-                console.log(response);
-                console.log("=== onSuccess: End");
+  const initSocure = (deviceSessionId: string): void => {
+    let config = {
+      onProgress: async (response: any) => {
+        console.log("=== onProgress: Start");
+        console.log(response);
+        console.log("=== onProgress: Start");
+      },
+      onSuccess: async (response: any) => {
+        response.token = token;
 
-                window.parent.postMessage({
-                    target: "tokenUpdate"
-                },"*" );
-            },
-            onError: async (response: any) => {
-                console.log("=== onError: Start");
-                console.log(response);
-                console.log("=== onError: End");
-            },
+        console.log("=== onSuccess: Start");
+        console.log(response);
+        console.log("=== onSuccess: End");
 
-            qrCodeNeeded: true
-        }
+        window.parent.postMessage({
+          target: "tokenUpdate"
+        }, "*");
+      },
+      onError: async (response: any) => {
+        console.log("=== onError: Start");
+        console.log(response);
+        console.log("=== onError: End");
+      },
 
-        SocureInitializer.init(API_KEY)
-            .then((lib: { init: (arg0: string, arg1: string, arg2: { onProgress: (progress: any) => void; onSuccess: (success: any) => void; onError: (error: any) => void; qrCodeNeeded: boolean; }) => Promise<any>; start: (arg0: number, p?: { customerUserId: string | undefined }) => Promise<any>; }) => {
-                lib.init(API_KEY, "#socure", config).then(function () {
-                    lib.start(2, {customerUserId: token}).then(function (response: any) {
-                            console.log('response: START');
-                            console.log(response);
-                            console.log('response: END')
-                        },
-                        function (error: any) {
-                            console.log('error: START');
-                            console.log(error);
-                            console.log('error: END')
-                        }
-                    );
-                });
-            });
+      qrCodeNeeded: true
     }
+    SocureInitializer.init(API_KEY)
+      .then((lib: {
+        init: (arg0: string, arg1: string, arg2: {
+          onProgress: (progress: any) => void;
+          onSuccess: (success: any) => void;
+          onError: (error: any) => void;
+          qrCodeNeeded: boolean;
+        }) => Promise<any>;
+        start: (arg0: number, p?: { modules: ['devicerisk'], customerUserId: string | undefined, deviceSessionId: string | undefined }) => Promise<any>;
+      }) => {
+        lib.init(API_KEY, "#socure", config).then(function () {
+          lib.start(2, {
+            modules: ['devicerisk'],
+            customerUserId: token,
+            deviceSessionId: deviceSessionId
+          }).then(function (response: any) {
+              console.log('response: START');
+              console.log(response);
+              console.log('response: END')
+            },
+            function (error: any) {
+              console.log('error: START');
+              console.log(error);
+              console.log('error: END')
+            }
+          );
+        });
+      });
+  }
 
-    let initialized = false;
-    useEffect(() => {
-        if (initialized) return;
+  let initialized = false;
+  useEffect(() => {
+    if (initialized) return;
 
-        // eslint-disable-next-line
-        initialized = true;
+    // eslint-disable-next-line
+    initialized = true;
 
-        start();
-    }, []);
+    start();
+  }, []);
 
-    return (
-        confirmed ? <Navigate to="/pay"/> :
-            <main style={{padding: "1rem 0"}} id="socure">
-                Initializing Document Verification Process
-            </main>
-    );
+  return (
+    confirmed ? <Navigate to="/pay"/> :
+      <main style={{padding: "1rem 0"}} id="socure">
+        Initializing Document Verification Process
+      </main>
+  );
 };
